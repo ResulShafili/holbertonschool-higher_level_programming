a85491bbef33e2b86f48f00b191b71364daa66c9
ResulShafili <qiyasogluzefer@gmail.com>
2026-02-12 21:17:31 +0000

    heyyyy you

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\ language that combines remarkable power with very clear syntax"
-print(str[55:72] + " " + str[0:6])
+print("object-oriented programming with Python")
 #cdc
 #dsx
+#fd
