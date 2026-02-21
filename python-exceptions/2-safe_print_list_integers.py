c2d14134938428519dae51ebadcbae2a5b906cad
ResulShafili <qiyasogluzefer@gmail.com>
2026-02-21 13:51:44 +0000

    2-safe_print_list_integers.py

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-kdef safe_print_list_integers(my_list=[], x=0):
+def safe_print_list_integers(my_list=[], x=0):
     count = 0
     i = 0
-    while True:
+    while i < x:
         try:
-            if i >= x:
-                break
             print("{:d}".format(my_list[i]), end="")
             count += 1
         except (ValueError, TypeError):
