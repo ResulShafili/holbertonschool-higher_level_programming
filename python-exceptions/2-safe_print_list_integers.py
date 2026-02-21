8a8a5d757b84f27d46847f91ace5364d2169ae4d
ResulShafili <qiyasogluzefer@gmail.com>
2026-02-21 13:49:00 +0000

    2-safe_print_list_integers.py

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-def safe_print_list_integers(my_list=[], x=0):
+kdef safe_print_list_integers(my_list=[], x=0):
     count = 0
     i = 0
-    while i < x:
+    while True:
         try:
+            if i >= x:
+                break
             print("{:d}".format(my_list[i]), end="")
             count += 1
         except (ValueError, TypeError):
-            # Əgər element tam ədəd deyilsə, keç
             pass
         except IndexError:
-            # Siyahı bitdisə, dayandır
             break
         i += 1
     print()
