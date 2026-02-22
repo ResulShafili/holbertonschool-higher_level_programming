1fa62f55afa763b83bdf7f4589b6015605cedde2
ResulShafili <qiyasogluzefer@gmail.com>
2026-02-22 13:43:34 +0000

    Add matrix_divided function and comprehensive doctests

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
 """
-Bu modul matrisin elementlərini bölən funksiyanı ehtiva edir.
-Bütün elementlər rəqəm (int/float) olmalıdır.
+This module contains the matrix_divided function.
+It divides all elements of a matrix by a given divisor.
 """
 
 
 def matrix_divided(matrix, div):
     """
-    Matrisin bütün elementlərini div-ə bölür.
-
-    Args:
-        matrix: Siyahıların siyahısı (tam ədədlər və ya float-lar).
-        div: Bölən rəqəm (sıfırdan fərqli).
-
-    Returns:
-        Bölünmüş elementlərdən ibarət yeni matris.
-
-    Raises:
-        TypeError: Girişlər düzgün tipdə olmadıqda.
-        ZeroDivisionError: div 0 olduqda.
+    Divides all elements of a matrix by div, rounded to 2 decimal places.
     """
     msg = "matrix must be a matrix (list of lists) of integers/floats"
 
-    if not isinstance(matrix, list) or not matrix:
+    if not isinstance(matrix, list) or len(matrix) == 0:
         raise TypeError(msg)
+
+    for row in matrix:
+        if not isinstance(row, list):
+            raise TypeError(msg)
+        for item in row:
+            if not isinstance(item, (int, float)):
+                raise TypeError(msg)
+
+    row_len = len(matrix[0])
+    if not all(len(row) == row_len for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
 
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
@@ -31,29 +31,4 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    # div NaN olarsa rədd et
-    if div != div:
-        raise TypeError("div must be a number")
-
-    row_len = None
-    new_matrix = []
-
-    for row in matrix:
-        if not isinstance(row, list) or not row:
-            raise TypeError(msg)
-        if row_len is None:
-            row_len = len(row)
-        elif len(row) != row_len:
-            raise TypeError("Each row of the matrix must have the same size")
-
-        new_row = []
-        for x in row:
-            if not isinstance(x, (int, float)):
-                raise TypeError(msg)
-            # Matris daxilində NaN və ya Inf-i rədd edirik
-            if x != x or abs(x) > 1.7976931348623158e+308:
-                raise TypeError(msg)
-            new_row.append(round(x / div, 2))
-        new_matrix.append(new_row)
-
-    return new_matrix
+    return [[round(x / div, 2) for x in row] for row in matrix]
