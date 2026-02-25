09935b2bcbfa4784da8c5d7bb711d239a6220a40
ResulShafili <qiyasogluzefer@gmail.com>
2026-02-26 00:37:50 +0400

    Create 2-rectangle.py

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+Class rectangle
+"""
+
+
+class Rectangle:
+    """represents Rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """
+        Creates rectangle object
+        Args:
+            width (int):
+            height (int):
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """This is Getter."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """This is setter for Width."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """This is Getter height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """This is setter for Height."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+    def area(self):
+      """Area of Rectangle   (width*height)"""
+      return self.__with* self.__height
+    def perimeter(self):
+      if self.__width==0 or self.__height==0:
+        return 0
+      else:
+        return 2*(self.__with* self.__height)
