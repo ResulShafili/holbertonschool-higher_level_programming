e0f6af757c7b020d817bb64e593ed3ddbc738ee9
ResulShafili <qiyasogluzefer@gmail.com>
2026-02-26 02:33:18 +0400

    Update 6-rectangle.py

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -7,6 +7,7 @@ Class rectangle
 class Rectangle:
     """represents Rectangle"""
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """
         Creates rectangle object
