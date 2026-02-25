8e076e0ad0ddd95d33310937b567c115c449a93e
ResulShafili <qiyasogluzefer@gmail.com>
2026-02-26 00:56:12 +0400

    Implement area and perimeter methods
    Add area and perimeter methods to Rectangle class

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -44,9 +44,11 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+        
     def area(self):
         """Area of Rectangle   (width*height)"""
         return self.__width * self.__height
+        
     def perimeter(self):
         """
         perimeter of the rectangle if h or w
