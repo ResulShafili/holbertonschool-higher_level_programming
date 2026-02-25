a5cc5399395cd094743e30f17ba508b5c2ad788a
ResulShafili <qiyasogluzefer@gmail.com>
2026-02-26 02:15:20 +0400

    Update 4-rectangle.py

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -70,65 +70,7 @@ class Rectangle:
             if i < self.__height - 1:
                 rect_str += "\n"
         return rect_str
-    #!/usr/bin/python3
-"""
-Bu modul Rectangle sinfini və onun __repr__ metodunu təyin edir.
-"""
-
-
-class Rectangle:
-    """Dördbucaqlını təmsil edən sinif."""
-
-    def __init__(self, width=0, height=0):
-        """Yeni bir dördbucaqlı yaradır."""
-        self.width = width
-        self.height = height
-
-    @property
-    def width(self):
-        """Eni geri qaytarır."""
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """Eni təyin edir və xətaları yoxlayır."""
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
-
-    @property
-    def height(self):
-        """Hündürlüyü geri qaytarır."""
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """Hündürlüyü təyin edir və xətaları yoxlayır."""
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
-
-    def area(self):
-        """Sahəni qaytarır."""
-        return self.__width * self.__height
-
-    def perimeter(self):
-        """Perimetri qaytarır."""
-        if self.__width == 0 or self.__height == 0:
-            return 0
-        return 2 * (self.__width + self.__height)
-
-    def __str__(self):
-        """Dördbucaqlını # simvolları ilə string kimi qaytarır."""
-        if self.__width == 0 or self.__height == 0:
-            return ""
-        rect_lines = ["#" * self.__width for _ in range(self.__height)]
-        return "\n".join(rect_lines)
 
     def __repr__(self):
-        """Obyektin eval() ilə yenidən yaradıla bilən string təsvirini qaytarır."""
+        """shows object wıth eval."""
         return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
