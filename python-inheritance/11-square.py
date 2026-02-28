b4087e00b1cd781aefbb7c9dfee69d88c74f4025
ResulShafili <qiyasogluzefer@gmail.com>
2026-03-01 02:50:30 +0400

    Update 11-square.py

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -28,4 +28,4 @@ class Square(Rectangle):
         """
         Returns a string representation of the rectangle.
         """
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+        return "[Square] {}/{}".format(self.__width, self.__height)
