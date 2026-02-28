3acf9cfcf62144fd1cde8e29921da31e1197530e
ResulShafili <qiyasogluzefer@gmail.com>
2026-03-01 00:07:53 +0400

    Create 6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+this is base  geometry
+"""
+
+class BaseGeometry:
+   """
+   this class is for geometry
+   """
+  def area(self):
+     """
+     This function gives area
+     """
