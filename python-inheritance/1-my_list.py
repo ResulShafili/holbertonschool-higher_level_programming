9e2226c43d31fc2708f833df77a38a1baacf56fb
ResulShafili <qiyasogluzefer@gmail.com>
2026-02-28 16:29:38 +0400

    Create 1-my_list.py

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+short wit inheritence 
+"""
+
+
+class MyList(list):
+    """
+    class my list
+    """
+
+    def print_sorted(self):
+        """
+        acsending short
+        """
+        print(sorted(self))
