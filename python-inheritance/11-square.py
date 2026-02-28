5608b735088ff52e110be84bcd766c9c053e81b7
ResulShafili <qiyasogluzefer@gmail.com>
2026-03-01 02:48:26 +0400

    Create 11-square.py

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This module defines a class Square that inherits from Rectangle.
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square class that inherits from Rectangle.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes the square with size.
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """
+        Calculates and returns the area of the square.
+        """
+        return self.__size ** 2
+
+    def __str__(self):
+        """
+        Returns a string representation of the rectangle.
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
