89fab6bd2406f4efb75354b2d042d98106f2aa42
ResulShafili <qiyasogluzefer@gmail.com>
2026-03-01 01:56:22 +0400

    Create 8-rectangle.py

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,21 @@
+#!/ust/bin/python3
+"""
+#Resul Shafili 
+#8-rectangle.py
+I created class Reactangle and worked some integer values 
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+class Rectangle(BaseGeometry):
+    """
+    Class for Rectangle which one is inherited by BaseGeometry
+    """
+    def __init__(self, width, height):
+        """
+        this function gives integer values only
+        """
+        self.integer_validator("width", width)
+        self.__width=width
+        self.integer_validator("height", height)
+        self.__height=height
