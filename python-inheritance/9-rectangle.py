141959a2aed5f82ccfb828bfb9a50e699e1bae12
ResulShafili <qiyasogluzefer@gmail.com>
2026-03-01 02:21:16 +0400

    Update 9-rectangle.py

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -29,4 +29,4 @@ class Rectangle(BaseGeometry):
         """
         Returns a string representation of the rectangle.
         """
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)        
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
