e76cbb7da56c10e1900cc2b0e8493f2c3976c0ca
ResulShafili <qiyasogluzefer@gmail.com>
2026-03-01 20:47:26 +0400

    Update task_00_abc.py

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import abc from ABC
+import abc from ABC, abstractmethod
 """
 this program defines sound of animals
 """
@@ -9,7 +9,7 @@ class Animal(ABC):
     this is anaimal class
     """
     @abstractmethod
-    def sound():
+    def sound(self):
         """
         this is abstarct sound metheod
         """
@@ -17,12 +17,12 @@ class Animal(ABC):
 
 class Dog(Animal):
     """this is class dog which is inherited from Animal class"""
-    def sound():
+    def sound(self):
         """this is mehtod sound for dog class"""
         return "Bark"
 
 class Cat(Animal):
     """this is class cat which is inherited from Animal class"""
-    def sound():
+    def sound(self):
         """this is mehtod sound for cat class"""
         return "Meow"
