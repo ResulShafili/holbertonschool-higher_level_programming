e36301c192fbb8db56e47672d5a892d2845623fe
ResulShafili <qiyasogluzefer@gmail.com>
2026-03-05 00:52:03 +0400

    Create 9-student.py

---
+++ python-input_output/9-student.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name=name
+        self.last_name=last_name
+        self.age=age
+
+    def class_to_json(obj):
+    """
+    return dict object
+    """
+    return obj.__dict__
+    
