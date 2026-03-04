8be0bc21f81b5a55fa633fdb02252ba3f4ce02a1
ResulShafili <qiyasogluzefer@gmail.com>
2026-03-04 23:19:56 +0400

    Create 4-from_json_string.py

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""this code converts json to the object(python)"""
+import json
+
+
+def from_json_string(my_str):
+    """convert to object method"""
+    return json.loads(my_obj)
