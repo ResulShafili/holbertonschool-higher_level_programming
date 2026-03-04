e8ce655db128c8ad48c7864acb8acb00caf8d9ae
ResulShafili <qiyasogluzefer@gmail.com>
2026-03-04 23:13:20 +0400

    Update 3-to_json_string.py

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import json
 """this code converts json to the object(python)"""
+import json
 
 
 def to_json_string(my_obj):
     """convert to object method"""
-    return my_obj.loads()
+    return json.loads(my_obj)
