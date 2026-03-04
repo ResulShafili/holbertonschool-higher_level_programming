3080659f7c75d5289103dcdd7ec5d20131c48148
ResulShafili <qiyasogluzefer@gmail.com>
2026-03-04 17:50:02 +0400

    Create 1-write_file.py

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""this code writes our text"""
+
+
+def write_file(filename="", text=""):
+    """def function"""
+    
+    with open(filename, "w", encoding="utf-8) as f:
+        """create file"""
+        f.write(text)
+    
