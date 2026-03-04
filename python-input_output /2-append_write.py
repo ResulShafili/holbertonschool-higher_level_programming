0420f60ea1bebe940732983744214fcb58cb45dd
ResulShafili <qiyasogluzefer@gmail.com>
2026-03-04 23:04:30 +0400

    Delete python-input_output  directory

--- python-input_output /2-append_write.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""this program adds somethin atthe end of text"""
-def append_write(filename="", text=""):
-    """metheod for adding"""
-    with open(filename, "a", encoding="utf-8") as f:
-        """i am opening file here for add text"""
-        f.write(text)
