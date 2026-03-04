ee63191fe2fe62266b9bb3cd46a1e8185318889d
ResulShafili <qiyasogluzefer@gmail.com>
2026-03-04 17:41:18 +0400

    Update 0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """Reads a UTF8 text file and prints it to stdout."""
     with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")"")
+        print(f.read(), end="")
