281156836a6afad51a43c65e5161dc908e04d2c8
ResulShafili <qiyasogluzefer@gmail.com>
2026-03-04 23:06:31 +0400

    Update 2-append_write.py

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """this program adds somethin atthe end of text"""
+
+
 def append_write(filename="", text=""):
     """metheod for adding"""
     with open(filename, "a", encoding="utf-8") as f:
