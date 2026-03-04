6133e603fc84b4d23ddccd25b5b57acf7cf745a4
ResulShafili <qiyasogluzefer@gmail.com>
2026-03-05 01:02:03 +0400

    Update 9-student.py

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -6,13 +6,12 @@ class Student:
     """class Strudent"""
     def __init__(self, first_name, last_name, age):
         """init method"""
-        self.first_name=first_name
-        self.last_name=last_name
-        self.age=age
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
         """
         return dict object
         """
         return self.__dict__
-    
