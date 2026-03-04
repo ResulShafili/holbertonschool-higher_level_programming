3ae82b85bfbfca3f9b82b651119defa751df3baa
ResulShafili <qiyasogluzefer@gmail.com>
2026-03-05 00:53:42 +0400

    Update 9-student.py

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""code starts"""
+
+
 class Student:
+    """class Strudent"""
     def __init__(self, first_name, last_name, age):
+        """init method"""
         self.first_name=name
         self.last_name=last_name
         self.age=age
