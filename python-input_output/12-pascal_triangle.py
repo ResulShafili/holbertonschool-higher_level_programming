954628d4ff47437deed65f6df3c4f10bc31a7dfd
ResulShafili <qiyasogluzefer@gmail.com>
2026-03-05 16:39:39 +0400

    Update 12-pascal_triangle.py

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -12,10 +12,8 @@ def pascal_triangle(n):
     for i in range(1, n):
         prev_row = triangle[-1]
         row = [1]
-        
         for j in range(1, i):
             row.append(prev_row[j-1] + prev_row[j])
-            
         row.append(1)
         triangle.append(row)
 
