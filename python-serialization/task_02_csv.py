3fa0cbf30ebdddfd7fa38383b3141eed67d07b0c
ResulShafili <qiyasogluzefer@gmail.com>
2026-03-08 14:06:32 +0400

    Update task_02_csv.py

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -14,7 +14,6 @@ def convert_csv_to_json(filename):
             info = list(csv.DictReader(csvf))
         with open("data.json", mode="w", encoding="utf-8") as jsf:
             x = json.dump(info, jsf, indent=4)
+        return True
     except Exception:
         return None
-        break
-    return True 
