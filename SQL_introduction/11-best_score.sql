f554bcd87721540d54069bddc61131eb291ed8db
ResulShafili <qiyasogluzefer@gmail.com>
2026-03-14 23:15:54 +0400

    Update 11-best_score.sql

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- ResulS Shafili
 -- this code gives all records of score
-SELECT score, name FROM second_table WHERE score >= 10 ORDER BY DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
