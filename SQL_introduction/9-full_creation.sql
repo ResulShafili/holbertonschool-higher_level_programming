34f1263f8e4045775c803272cda4b58292ccb2bc
ResulShafili <qiyasogluzefer@gmail.com>
2026-03-14 22:57:37 +0400

    Create 9-full_creation.sql

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,14 @@
+-- ResulShafili
+-- this code creates ne table and adds new variables
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+INSERT INTO second_table (id, name, score)
+VALUES 
+    (1, "John", 10),
+    (2, "Alex", 3),
+    (3, "Bob", 14),
+    (4, "George", 8);
