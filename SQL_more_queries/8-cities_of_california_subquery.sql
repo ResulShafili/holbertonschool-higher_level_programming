8d9f3965754a1fe674f1bdb2eefdf5c1e060f5eb
ResulShafili <qiyasogluzefer@gmail.com>
2026-03-15 17:28:12 +0400

    Create 8-cities_of_california_subquery.sql

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,10 @@
+--ResulShafili
+--this code finds in califorina db
+SELECT id, name 
+FROM cities 
+WHERE state_id = (
+    SELECT id 
+    FROM states 
+    WHERE name = 'California'
+) 
+ORDER BY id ASC;
