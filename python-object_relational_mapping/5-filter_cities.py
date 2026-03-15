1b564f405f65817542ff3f2cd79d4dc65565dd18
ResulShafili <qiyasogluzefer@gmail.com>
2026-03-15 18:33:29 +0000

    Add filter cities script

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Lists all cities of a state from the database hbtn_0e_4_usa"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                           passwd=password, db=database, charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT cities.name FROM cities "
+                "JOIN states ON cities.state_id = states.id "
+                "WHERE states.name = %s "
+                "ORDER BY cities.id ASC", (state_name,))
+    query_rows = cur.fetchall()
+    print(", ".join([row[0] for row in query_rows]))
+    cur.close()
+    conn.close()
