e036835372ab734eaaf9f744960d6da6551020df
ResulShafili <qiyasogluzefer@gmail.com>
2026-03-15 18:15:59 +0000

    Add filter states scripts

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Lists all states starting with N from the database hbtn_0e_0_usa"""
+"""Lists all states matching the argument from the database hbtn_0e_0_usa"""
 import sys
 import MySQLdb
 
@@ -7,10 +7,12 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
+    state_name = sys.argv[4]
     conn = MySQLdb.connect(host="localhost", port=3306, user=username,
                            passwd=password, db=database, charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name = '{}' \
+ORDER BY id ASC".format(state_name))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
