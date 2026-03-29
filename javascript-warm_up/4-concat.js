d41a1005e421116b3544502727ae3e0c486fd561
ResulShafili <qiyasogluzefer.com>
2026-03-29 19:00:44 +0000

    fix: fix 4-concat.js content and permission

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,7 +1,2 @@
 #!/usr/bin/node
-
-const process = require('process');
-const args = process.argv;
-if (args.length >= 2) {
-  console.log(`${args[2]} is ${args[3]}`);
-}
+console.log(process.argv[2] + ' is ' + process.argv[3]);
